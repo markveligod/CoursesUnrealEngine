e8c1147050e40498a1d615ab084151cc5f86f5cb
Mark <markveligod@yandex.ru>
2020-12-11 15:58:56 +0300

    add

--- Module_02/main.cpp
+++ Module_02/main.cpp
@@ -29,23 +29,62 @@ void Varibles()
 void ConstVaribles()
 {
 	std::cout << "void ConstVaribles()\n";
-	const int a = 4;
-	std::cout << "a = " << a << std::endl;
-	const int b = 14;
-	std::cout << "b = " << b << std::endl;
-	const int c = 114;
-	std::cout << "c = " << c << std::endl;
-	std::cout << "a + b + c = " << a + b + c << std::endl;
-	std::cout << "a - b - c = " << a - b - c << std::endl;
-	std::cout << "a * b * c = " << a * b * c << std::endl;
-	std::cout << "a / b / c = " << a / b / c << std::endl;
+	const int CodeA = 4;
+	std::cout << "CodeA = " << CodeA << std::endl;
+	const int CodeB = 14;
+	std::cout << "CodeB = " << CodeB << std::endl;
+	const int CodeC = 114;
+	std::cout << "CodeC = " << CodeC << std::endl;
+	std::cout << "CodeA + CodeB + CodeC = " << CodeA + CodeB + CodeC << std::endl;
+	std::cout << "CodeA - CodeB - CodeC = " << CodeA - CodeB - CodeC << std::endl;
+	std::cout << "CodeA * CodeB * CodeC = " << CodeA * CodeB * CodeC << std::endl;
+	std::cout << "CodeA / CodeB / CodeC = " << CodeA / CodeB / CodeC << std::endl;
+}
+
+void InputTerminal()
+{
+	std::cout << "void InputTerminal()\n";
+	int GuessA, GuessB, GuessC;
+
+	std::cin >> GuessA >> GuessB >> GuessC;
+	std::cout << "Your enter: " << GuessA << " " << GuessB << " " << GuessC << std::endl;
+	int GuessSum = GuessA + GuessB + GuessC;
+	int GuessMulti = GuessA * GuessB * GuessC;
+	std::cout << "GuessSum: " << GuessSum << std::endl;
+	std::cout << "GuessMulti: " << GuessMulti << std::endl;
+}
+
+void IfState()
+{
+	std::cout << "void IfState()\n";
+	if (true)
+	{
+		std::cout << "TRUE!\n";
+	}
+
+	if (false)
+	{
+		std::cout << "LOL WHAT!\n";
+	}
+	else
+	{
+		std::cout << "FALSE!\n";
+	}
+	
 }
 
 int main(void)
 {
 	//HelloWorld();
-	Intro();
-	Varibles();
-	ConstVaribles();
+	while (true)
+	{
+		Intro();
+		Varibles();
+		ConstVaribles();
+		InputTerminal();
+		IfState();
+		std::cin.clear();
+		std::cin.ignore();
+	}
 	return (0);
 }
--- README.md
+++ README.md
@@ -23,10 +23,14 @@ The course aim to learn base c++ language and him use in Unreal Engine 4. The co
 The module is an introduction to course and show information about support on forums, discord, twitter and facebook. Also the module tell you about CEO GameDev.tv and him teams. The module help to you setup all necessary tools which will you need in the next modules. Be careful...  
 
 ### :black_square_button: 2. Triple X - Write Pure C++.  
-- [x] (Test #1: Triple X Mid-section Quiz)  
+- [x] (Test #1: Triple X Start-section Quiz)  
 The test include some part module: Variable, const Variable, include, declared statement, expression statements and comments.  
   
-- [ ] (Test #2: Triple X Mid-section Quiz)  
+- [x] (Test #2: Triple X Mid-section Quiz)  
+The test include some part module: naming and self documenting code, using if and else statements, functions and etc.  
+  
+- [ ] (Test #3: Triple X End-section Quiz)  
+(very soon)  
   
 ### :black_square_button: 3. Bulls & Cows.  
 (very soon)  
