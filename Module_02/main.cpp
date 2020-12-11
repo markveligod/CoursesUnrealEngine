ebf0ded1384461d7244c173f55f5ecfc5d611331
Mark <markveligod@yandex.ru>
2020-12-11 10:28:11 +0300

    fix VS code

--- Module_02/main.cpp
+++ Module_02/main.cpp
@@ -11,9 +11,20 @@ void Intro()
 	std::cout << "You need to enter the correct codes to continue...\n";
 }
 
+void Varibles()
+{
+	int a = 4;
+	std::cout << "a = " << a << std::endl;
+	int b = 14;
+	std::cout << "a = " << b << std::endl;
+	int c = 114;
+	std::cout << "a = " << c << std::endl;
+}
+
 int main(void)
 {
 	//HelloWorld();
 	Intro();
+	Varibles();
 	return (0);
 }
