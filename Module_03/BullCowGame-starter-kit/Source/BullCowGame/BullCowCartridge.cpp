8485ec62880970e550cfdc7d57de5c4b3f931206
Mark <markveligod@yandex.ru>
2020-12-15 11:12:18 +0300

    add complete Test 5

--- Module_03/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ Module_03/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,13 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to BULL COWS"));
+    PrintLine(TEXT("Press enter to continue..."));
+    
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
+    FString HiddenWord = TEXT("cake");
 }
